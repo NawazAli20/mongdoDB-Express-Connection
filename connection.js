3273e528d1ca19108dbd6fc962c1ed64073e10af
Nawaz.Ali20 <gga@g.clemson.edu>
2026-05-23 12:02:51 -0500

    update in server.js

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 async function Connection(){
     try{
-        mongoose.connect("mongodb://127.0.0.1:27017/DemoDB1"); 
+       await  mongoose.connect("mongodb://127.0.0.1:27017/DemoDB1"); 
         console.log("MongoDB is connected at port 27017");
     }catch(err){
         console.log("MongoDB connection error: "+err);
